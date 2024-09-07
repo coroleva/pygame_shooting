2bd1f377bfb9577716b34c34044f2b738d6ae0f7
Юлия <coroleva.yu@gmail.com>
2024-09-07 21:07:34 +0300

    add all circle code

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Игра Тир')
 
-icon = pygame.image.load('img/icon.png')
+icon = pygame.image.load('img/тир.png')
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load('img/icon.png')
+target_img = pygame.image.load('img/тир.png')
 target_width = 50
 target_height = 50
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
@@ -20,6 +20,22 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+    pygame.draw.rect(screen, (0, 0, 0), (target_x, target_y, target_width, target_height))
+
+
+
 
 pygame.quit()
